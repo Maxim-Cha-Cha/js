f49c7f314c77e28652a34956ebe515d55bb0a450
Maxim <by.maksim01@gmail.com>
2022-01-28 20:41:18 +0200

    123

--- index.js
+++ index.js
@@ -1,26 +1,73 @@
-const numbername = function (number) {
-  if (number % 2 === 0) {
-    return "true";
+const trafficColor = function (colorname) {
+  const color1 = "Красный";
+  const color2 = "Жёлтый";
+  const color3 = "Зелёный";
+  if (colorname === color1) {
+    return "Стой";
+  } else if (colorname === color2) {
+    return "Приготовся";
+  } else if (colorname === color3) {
+    return "Можно - продолжать - движение";
   } else {
-    return "false";
-  }
-};
-const number1 = +prompt("Enter namber");
-const result = numbername(number1);
-console.log(result);
-
-const userNamber = function (num1, num2) {
-  if (num1 > num2) {
-    return num2;
-  } else {
-    return num1;
+    return "Введите - цвет";
   }
 };
 
-const money = function(num1, num2){
-  if(num1 => num2){
-    return 'Вам хватает денег'
-  }else {
-    return 'Вам не хватает денег'
+const month = "user-month";
+switch (month) {
+  case "12":
+  case "1":
+  case "2": {
+    console.log("Зима");
+    break;
   }
+  case "3":
+  case "4":
+  case "5": {
+    console.log("Весна");
+    break;
+  }
+  case "6":
+  case "7":
+  case "8": {
+    console.log("Лето");
+    break;
+  }
+  case "9":
+  case "10":
+  case "11": {
+    console.log("Осень");
+    break;
+  }
+  default: {
+    console.log("Введите число месяца");
+    break;
+  }
+};
+
+const x = +prompt('Введите x');
+const y = +prompt('Введите y');
+const operator = prompt('Введите оператор');
+function calculator(x, y, operator){
+const result;
+  switch(operator){
+    case'+':{
+     result = x + y;
+      break;
+    }
+    case'-':{
+      result = x - y;
+      break;
+    }
+    case'*':{
+      result = x * y;
+      break;
+    }
+    case'/':{
+      result = x / y;
+      break;
+    }
+  
+  }
+  return result
 }
